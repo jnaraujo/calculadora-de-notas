f5ce2f250f72399af5c7fe92afb19845683d0212
Jonatas A <jonatasaraujofx@gmail.com>
2022-06-24 00:13:34 -0300

    feat: preact support

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,10 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig, splitVendorChunkPlugin } from "vite";
+import preact from "@preact/preset-vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  build: {
+    minify: "terser",
+  },
+  plugins: [preact(), splitVendorChunkPlugin()],
 });

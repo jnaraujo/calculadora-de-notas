f87ff505ecc2f5e6fe68be352f799f12cfd9ff43
Jonatas A <jonatasaraujofx@gmail.com>
2022-06-23 22:28:16 -0300

    updates

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,7 @@ import { useForm } from "react-hook-form";
 import "./App.scss";
 
 function App() {
-  const {
-    register,
-    handleSubmit,
-    watch,
-    formState: { errors },
-  } = useForm();
+  const { register, watch, setValue } = useForm();
 
   const [notaNecessaria, setNotaNecessaria] = useState(10);
 
@@ -24,10 +19,22 @@ function App() {
     const subscription = watch((value) => {
       const notasUsuario = [value.primeira, value.segunda];
 
+      if (value.primeira > 10) {
+        return setValue("primeira", 10);
+      }
+      if (value.segunda > 10) {
+        return setValue("segunda", 10);
+      }
+      if (value.primeira < 0) {
+        return setValue("primeira", 0);
+      }
+      if (value.segunda < 0) {
+        return setValue("segunda", 0);
+      }
+
       const notasPesos = {
         primeira: (Number(notasUsuario[0]) * pesos[1]) / 8,
         segunda: (Number(notasUsuario[1]) * pesos[2]) / 8,
-        terceira: (5.8 * pesos[3]) / 8,
       };
 
       const notaAtual = notasPesos.primeira + notasPesos.segunda;
@@ -43,10 +50,10 @@ function App() {
     <div className="App">
       <div className="Content">
         <div className="top">
-          <h1>Calculadora de nota em Pré Cálculo</h1>
+          <h1>Calculadora de nota em Pré-Cálculo</h1>
           <p>
             Para saber quanto você precisa tirar na última prova para ficar na
-            média de 7,
+            média de 7.
           </p>
         </div>
         <form>
@@ -74,13 +81,25 @@ function App() {
             />
           </div>
         </form>
-        <div
-          className={`finalnote ${
-            notaNecessaria <= 6.5 ? "good" : notaNecessaria <= 8 ? "ok" : "bad"
-          }`}
-        >
-          Você vai precisar tirar <span>{notaNecessaria.toPrecision(2)}</span>{" "}
-          na última prova para não ir pra final.
+        <div className="line"></div>
+        <div className={`finalnote`}>
+          <div>
+            <p>
+              Quanto você vai precisar tirar na última prova para não ir pra
+              final:
+            </p>
+          </div>
+          <div
+            className={`${
+              notaNecessaria <= 6.5
+                ? "good"
+                : notaNecessaria <= 8
+                ? "ok"
+                : "bad"
+            }`}
+          >
+            {notaNecessaria.toPrecision(2)}
+          </div>
         </div>
       </div>
     </div>
